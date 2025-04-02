8d74560756a20b0f9dada83b6df4b8937f9146ad
mikhailnefedov <52661281+mikhailnefedov@users.noreply.github.com>
2025-04-02 20:49:51 +0200

    Start implementing attribute

---
+++ JsonConstantsGenerator.Attributes/GenerateConstantsFromList.cs
@@ -0,0 +1,17 @@
+﻿namespace JsonConstantsGenerator.Attributes;
+
+/// <summary>
+/// 
+/// </summary>
+public class GenerateConstantsFromList : Attribute
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public string? FileName { get; set; }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public string? ClassName { get; set; }
+}
